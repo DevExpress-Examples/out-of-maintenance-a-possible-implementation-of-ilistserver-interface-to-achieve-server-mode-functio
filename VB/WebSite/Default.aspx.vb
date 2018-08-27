3bc4517d502322a830133b472ec309d7eeff4111
DevExpressExampleBot <fakeEmail>
2018-08-27 18:08:16 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -9,7 +9,12 @@ Partial Public Class _Default
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+        If Not IsPostBack Then
+            grid.DataBind()
+        End If
+    End Sub
+
+    Protected Sub grid_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
         grid.DataSource = New DataObject()
-        grid.DataBind()
     End Sub
 End Class
